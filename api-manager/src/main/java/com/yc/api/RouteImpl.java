1be91687bdf85a5b612a42f6b636b556d3eecff3
杨充 <yangchong211@163.com>
2021-03-09 16:35:44 +0800

    接口通信代码完善

--- api-manager/src/main/java/com/yc/api/RouteImpl.java
+++ api-manager/src/main/java/com/yc/api/RouteImpl.java
@@ -1,5 +1,10 @@
 package com.yc.api;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * <pre>
  *     @author 杨充
@@ -10,10 +15,10 @@ package com.yc.api;
  * </pre>
  */
 //@Retention用来修饰这是一个什么类型的注解。这里表示该注解是一个编译时注解。
-//@Retention(RetentionPolicy.CLASS)
+@Retention(RetentionPolicy.CLASS)
 //@Target用来表示这个注解可以使用在哪些地方。比如：类、方法、属性、接口等等。
 //这里ElementType.TYPE 表示这个注解可以用来修饰：Class, interface or enum declaration。
-//@Target(ElementType.TYPE)
+@Target(ElementType.TYPE)
 public @interface RouteImpl {
 
     /**
